867488d41babe8b39eda88daa958b039ca0c0e91
yumi-jun <junyumi1006@naver.com>
2022-04-28 15:40:05 +0900

    d

--- ARTeamProject/Assets/Question.cs
+++ ARTeamProject/Assets/Question.cs
@@ -7,11 +7,22 @@ using UnityEngine.XR.ARSubsystems;
 
 public class Question : MonoBehaviour
 {
-
+    private ARRaycastManager raycastManager;
     public LayerMask cubelayer;
     public List<GameObject> ACube = new List<GameObject>();
     Canvas canvas;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private bool tap = false;
+    private GameObject spawnedObject;
+    private Vector2 touchPosition;
+    [SerializeField]
+    private GameObject objectToInstantiate;
+    private ARPlaneManager mARPlaneManager;
 
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,33 +30,93 @@ public class Question : MonoBehaviour
         canvas = gameObject.GetComponentInChildren<Canvas>();
         canvas.enabled = false;
     }
-        // Update is called once per frame
-        void Update()
+    void Update()
+    {
+
+        // check input
+        if (TryGetTouchPosition(out touchPosition))
         {
+            raycastAndCreateAndUpdate();
+        }
+    }
+    // Update is called once per frame
+    private void raycastAndCreateAndUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(touchPosition);
 
-            if (Input.touchCount > 0)
+        // do raycast
+        if (raycastManager.Raycast(ray, hits, TrackableType.PlaneWithinPolygon))
+        {
+            // get position and rotation of the first plane hit and instantiate a gameobject
+            Pose hitPose = hits[0].pose;
+
+            // instantiate only one time
+            if (!tap)
             {
-                Debug.Log("touch!");
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                if (Physics.Raycast(ray, out hit, cubelayer))
+                if (spawnedObject == null)
                 {
-                    if (hit.collider.gameObject.name == ACube[0].name)
-                    {
-                        Debug.Log("True " + hit.collider.gameObject);
-                        canvas.enabled = true;
+                    spawnedObject = Instantiate(objectToInstantiate, hitPose.position, hitPose.rotation);
+                    // 삼지선다 답안 큐브 출력
+                    Instantiate(ACube[0], new Vector3(0, -1.5f, 4), hitPose.rotation);
+                    Instantiate(ACube[1], new Vector3(1.5f, -1.5f, 4), hitPose.rotation);
+                    Instantiate(ACube[2], new Vector3(3f, -1.5f, 4), hitPose.rotation);
 
-
-                    }
-                    else
-                    {
-                        Debug.Log("False " + hit.collider.gameObject);
-                    }
+                    DisabledPlaneDetection();
                 }
+                else
+                {
+                    // update position
+                    spawnedObject.transform.position = hitPose.position + Vector3.up * (spawnedObject.transform.localScale.y / 2);
+                }
+                tap = true;
             }
         }
     }
 
-    
+    //평면 인식 더 이상 하기 않게 설정
+    private void DisabledPlaneDetection()
+    {
+        mARPlaneManager.enabled = false;
+        foreach (ARPlane plane in mARPlaneManager.trackables)
+            plane.gameObject.SetActive(false);
+    }
+    private bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        // touch and drag
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+
+    }
+    private void QuestionCube()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, cubelayer))
+        {
+            if (Input.touchCount > 0)
+            {
+                if (hit.collider.gameObject.name == ACube[0].name)
+                {
+                    Debug.Log("True " + hit.collider.gameObject);
+                    canvas.enabled = true;
+
+
+                }
+                else
+                {
+                    Debug.Log("False " + hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
+
+
+
 
 
