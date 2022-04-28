d3e0f7cf1dbe945d6c784271010b3dfc7ba20742
Jin-hyeun-jun <yong5jin@ajou.ac.kr>
2022-04-28 21:35:34 +0900

    soma수정

--- ARTeamProject/Assets/Scripts/soma.cs
+++ ARTeamProject/Assets/Scripts/soma.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.ARFoundation;
 public class soma : MonoBehaviour
 {
+    public ARRaycastManager aRRaycaster;
     [SerializeField]
     private Camera playerCamera;
     int i=0;
     //private bool buildModeOn = false;
     private bool canBuild = false;
 
+    
     private BlockSystem bSys;
 
     [SerializeField]
     private LayerMask buildableSurfacesLayer;
 
+    private Collision detection;
     private Vector3 buildPos;
 
     private GameObject currentTemplateBlock;
@@ -28,7 +31,8 @@ public class soma : MonoBehaviour
     //private Material templateMaterial;
 
     private int blockSelectCounter = 0;
-
+    
+    
     private void Start()
     {
         bSys = GetComponent<BlockSystem>();
@@ -36,40 +40,34 @@ public class soma : MonoBehaviour
 
     private void Update()
     {
-        /*
-        if (Input.GetKeyDown("e"))
-        {
-            buildModeOn = !buildModeOn;
+      
 
-            if (buildModeOn)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-            }
-            else 
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }
-        }
-
-        if (Input.GetKeyDown("r"))
-        {
-            blockSelectCounter++;
-            if (blockSelectCounter >= bSys.allBlocks.Count) blockSelectCounter = 0;
-        }*/
-
-        //if (buildModeOn)
-        //{
-            RaycastHit buildPosHIt;
+        RaycastHit buildPosHIt;
+            
+              
 
             if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)), out buildPosHIt, 10, buildableSurfacesLayer))
             {
                 Vector3 point = buildPosHIt.point;
-            float gridSize = 0.1f;
+                 float gridSize = 0.1f;
                 //grid placement
-                buildPos = new Vector3(Mathf.Round(point.x/ gridSize) * gridSize, Mathf.Round(point.y/ gridSize) * gridSize +0.1f, Mathf.Round(point.z/ gridSize) * gridSize);
-                canBuild = true;
-                Debug.Log(buildPosHIt.collider.name);
+               buildPos = new Vector3(Mathf.Round(point.x/ gridSize) * gridSize, Mathf.Round(point.y/ gridSize) * gridSize +0.1f, Mathf.Round(point.z/ gridSize) * gridSize);
+                //canBuild = true;
+                /*
+            Touch touch = Input.GetTouch(0);
+
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            if (aRRaycaster.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes))
+            {
+                Pose hitPose = hits[0].pose;
+                buildPos = hitPose.position;
             }
+                */
+            canBuild = true;
+          
+            //OnTriggerEnter(buildPosHIt.collider.tag);
+            //Debug.Log(buildPosHIt.collider.name);
+        }
             
             else if(currentTemplateBlock != null) //else if로 추가하기
             {
@@ -77,13 +75,7 @@ public class soma : MonoBehaviour
                canBuild = false;
             }
             
-        //}
-        /*
-        if (!buildModeOn && currentTemplateBlock != null)
-        {
-            Destroy(currentTemplateBlock.gameObject);
-            canBuild = false;
-        }*/
+     
 
         if (canBuild && currentTemplateBlock == null)
         {
@@ -100,8 +92,10 @@ public class soma : MonoBehaviour
                 PlaceBlock();
             }*/
         }
+       
     }
 
+    
     public void PlaceBlock()
     {
         GameObject newBlock = Instantiate(blockPrefab[i], currentTemplateBlock.transform.position, Quaternion.identity);
@@ -140,5 +134,25 @@ public class soma : MonoBehaviour
                 Destroy(placedSoma);
         }
     }
+    public void touchmove()
+    {
+        Touch touch = Input.GetTouch(0);
 
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        if(aRRaycaster.Raycast(touch.position,hits,UnityEngine.XR.ARSubsystems.TrackableType.Planes))
+        {
+            Pose hitPose = hits[0].pose;
+        }
+    }
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PlacedSoma"))
+        {
+            Destroy(currentTemplateBlock.gameObject);
+
+        }
+    }
+   
 }
