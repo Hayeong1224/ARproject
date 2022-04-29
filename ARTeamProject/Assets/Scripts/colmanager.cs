765af0e1aa4d43d0f7cefaeb30bbbd0a0b38a82d
Jin-hyeun-jun <yong5jin@ajou.ac.kr>
2022-04-30 02:12:51 +0900

    fix soma

--- ARTeamProject/Assets/Scripts/colmanager.cs
+++ ARTeamProject/Assets/Scripts/colmanager.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class colmanager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   static public bool rendcheck;
+
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
     public void OnCollisionEnter(Collision collision)
     {
-      
+        if (collision.gameObject.CompareTag("PlacedBlock"))
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.GetComponent<Renderer>().enabled = false;
+            }
+            rendcheck = true;
+        }
     }
     public void OnCollisionExit(Collision collision)
     {
-       
+        if (collision.gameObject.CompareTag("PlacedBlock"))
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.GetComponent<Renderer>().enabled = true;
+            }
+            rendcheck = false;
+        }
     }
+
 }
